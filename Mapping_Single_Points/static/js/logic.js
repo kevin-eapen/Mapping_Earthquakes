c98bf1e309624ee971a66f0f31bb199abe4e866f
Kevin Eapen <kevineapen777@gmail.com>
2022-12-09 17:59:25 -0800

    adding circleMarker function.

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -7,7 +7,7 @@
 let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
 // Add a marker to the map for Los Angeles, California.
-L.circle([34.0522, -118.2437], {
+L.circleMarker([34.0522, -118.2437], {
     radius: 300,
     color: "black",
     fillColor: "#ffffa1"
