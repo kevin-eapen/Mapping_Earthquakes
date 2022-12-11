32562a2fac0cc03a8ccf3a2863503ff3471f7876
Kevin Eapen <kevineapen777@gmail.com>
2022-12-10 20:10:25 -0800

    Mapping Earthquakes from past 7 days (Step 1).

--- Earthquakes_past7days/static/js/logicStep1.js
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -1,7 +1,6 @@
 
 // Add console.log to check to see if our code is working
-// console.log("working");
-
+console.log("working");
 
 // We create the street view tile layer that will be the default background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v12/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -20,37 +19,30 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 // Create a base layer that holds both maps.
 let baseMaps = {
     "Streets": streets,
-    "Satellite Streets": satelliteStreets
+    "Satellite": satelliteStreets
   };
 
   // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-    center: [43.7, -79.3],
-    zoom: 11,
+    center: [39.5, -98.5],
+    zoom: 3,
     layers: [streets]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-// Accessing the Toronto neighborhoods polygons GeoJSON URL.
-let torontoHoods = "https://raw.githubusercontent.com/kevin-eapen/Mapping_Earthquakes/Mapping_GeoJSON_Polygons/Mapping_GeoJSON_Polygons/static/js/data/torontoNeighborhoods.json"
+// Accessing the past 7 days Earthquakes GeoJSON data URL.
+let past7daysEarthquakes = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
-// Create a style for the lines.
-let myStyle = {
-  color: "#0000ff",
-  weight: 1,
-  fillColor: "#ffffa1"
-}
 
-// Grabbing our GeoJSON data.
-d3.json(torontoHoods).then(function(data) {
+// Grabbing earthquake GeoJSON data.
+d3.json(past7daysEarthquakes).then(function(data) {
     console.log(data);
     // Creating a GeoJSON layer with the retrieved data.
   L.geoJSON(data, {
-    style: myStyle,
     onEachFeature: function(feature, layer) {
-      layer.bindPopup("<h3> Neighborhood: " + feature.properties.AREA_NAME + "</h3>");
+      layer.bindPopup("<h3> Magnitude: " + feature.properties.mag + "</h3>");
      }
 }).addTo(map);
 });
